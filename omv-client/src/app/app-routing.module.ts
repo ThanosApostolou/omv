43673236bc93905eab9f6d38d35b0ba1b741274e
ThanosApostolou <thanosapostolou@outlook.com>
2020-04-04 21:51:35 +0300

    client: add initial structure

--- omv-client/src/app/app-routing.module.ts
+++ omv-client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Index } from '../views/index.component';
+import { About } from '../views/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Index },
+  { path: 'about', component: About }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
