c4aa02ece72002ceeee59e5a3cb449eb32aa4835
thanosapostolou <thanosapostolou@outlook.com>
2020-04-18 17:12:04 +0300

    change favicon

--- omv-client/src/views/Home.vue
+++ omv-client/src/views/Home.vue
@@ -6,13 +6,6 @@
             </h1>
         </v-row>
         <v-row align='center' justify='center' class='text-center'>
-            <p>
-                {{ appdata.appName }} is an application where you can choose 2 owl files
-                and a json file with their mappings.<br>
-                The application will visualize this information in a understandable way.
-            </p>
-        </v-row>
-        <v-row align='center' justify='center' class='text-center'>
             <v-img
                 alt='Logo'
                 src='../assets/omv_logo.svg'
@@ -21,6 +14,13 @@
                 height='128'
                 width='128'
             />
+        </v-row>
+        <v-row align='center' justify='center' class='text-center'>
+            <p>
+                {{ appdata.appName }} is an application where you can choose 2 owl files
+                and a json file with their mappings.<br>
+                The application will visualize this information in a understandable way.
+            </p>
         </v-row>
         <v-row align='center' justify='center' class='text-center'>
             <p>
