207198738d5ffe3998407c9333628904eb767e21
thanosapostolou <thanosapostolou@outlook.com>
2020-08-29 12:59:07 +0300

    serever: add OwlObjectProperties

---
+++ omv-server/src/main/java/omv/server/entities/OwlObjectPropertyNode.java
@@ -0,0 +1,142 @@
+package omv.server.entities;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLSubObjectPropertyOfAxiom;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+public class OwlObjectPropertyNode {
+    public OWLOntology owlontology;
+    public OWLObjectProperty objprop;
+    public String iri;
+    public String name;
+    public String label;
+    public ArrayList<Annotation> annotations;
+    public ArrayList<OwlObjectPropertyNode> children;
+
+    public OwlObjectPropertyNode() {
+        this.owlontology = null;
+        this.objprop = null;
+        this.iri = null;
+        this.name = null;
+        this.label = null;
+        this.annotations = new ArrayList<Annotation>();
+        this.children = new ArrayList<OwlObjectPropertyNode>();
+    }
+    public void create(OWLObjectProperty objprop, OWLOntology ontology) {
+        this.owlontology = ontology;
+        this.objprop = objprop;
+        this.iri = objprop.toStringID();
+        this.name = this.iri.split("#")[1];
+        Stream<OWLAnnotationAssertionAxiom> found_annotations = this.owlontology.annotationAssertionAxioms(this.objprop.getIRI());
+        for (OWLAnnotationAssertionAxiom found_annotation : found_annotations.toArray(OWLAnnotationAssertionAxiom[]::new)) {
+            Annotation myannotation = Annotation.fromStrings(found_annotation.getProperty().toString(), found_annotation.getValue().toString());
+            this.annotations.add(myannotation);
+        }
+        this.label = Annotation.getLabel(this.annotations);
+        if (this.label.isEmpty()) {
+            this.label = this.iri;
+        }
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public void sortChildren() {
+        this.children.sort(Comparator.comparing(OwlObjectPropertyNode::getLabel));
+    }
+
+    public void addChild(OwlObjectPropertyNode child) {
+        this.children.add(child);
+    }
+
+    public void addSubObjectProperties() {
+        Stream<OWLSubObjectPropertyOfAxiom> subClassAxioms = this.owlontology.objectSubPropertyAxiomsForSuperProperty(this.objprop);
+        for (OWLSubObjectPropertyOfAxiom axiom : subClassAxioms.toArray(OWLSubObjectPropertyOfAxiom[]::new)) {
+            //if(axiom.getSubClass().getClassExpressionType().getName().equals("Class")) {
+                Stream<OWLObjectProperty> subObjectProperties = axiom.getSubProperty().objectPropertiesInSignature();
+                for (OWLObjectProperty subobjprop : subObjectProperties.toArray(OWLObjectProperty[]::new)) {
+                    OwlObjectPropertyNode subobjpropnode = new OwlObjectPropertyNode();
+                    subobjpropnode.create(subobjprop, this.owlontology);
+                    this.addChild(subobjpropnode);
+                }
+            //}
+        }
+        this.sortChildren();
+        for (OwlObjectPropertyNode child : this.children) {
+            child.addSubObjectProperties();
+        }
+    }
+
+    public JsonObject toJsonObject() {
+        JsonObject jsonobject = new JsonObject();
+        jsonobject.put("iri", this.iri);
+        jsonobject.put("name", this.name);
+        jsonobject.put("label", this.label);
+        jsonobject.put("annotations", Annotation.listToJsonArray(this.annotations));
+        JsonArray childrenJsonArray = new JsonArray();
+        for (OwlObjectPropertyNode child : this.children) {
+            childrenJsonArray.add(child.toJsonObject());
+        }
+        jsonobject.put("children", childrenJsonArray);
+        return jsonobject;
+    }
+
+    public String toString(String dashes) {
+        String result = dashes+this.label+"\n";
+        for (OwlObjectPropertyNode child : this.children) {
+            result += child.toString(dashes+"-");
+        }
+        return result;
+    }
+
+    public String toString() {
+        return this.toString("");
+    }
+
+    public void createRoot(OWLOntology owlontology) {
+        this.owlontology = owlontology;
+        this.name = "owl:topObjectProperty";
+        this.iri = "owl:topObjectProperty";
+        this.label = "owl:topObjectProperty";
+        for (OWLObjectProperty objprop : this.owlontology.objectPropertiesInSignature().toArray(OWLObjectProperty[]::new)) {
+            OwlObjectPropertyNode objpropnode = new OwlObjectPropertyNode();
+            objpropnode.create(objprop, this.owlontology);
+            if (!objpropnode.name.equals("topObjectProperty")) {
+                Stream<OWLSubObjectPropertyOfAxiom> superObjectPropertyAxioms = this.owlontology.objectSubPropertyAxiomsForSubProperty(objprop);
+                AtomicBoolean istoplevel = new AtomicBoolean();
+                istoplevel.set(true);
+                for (OWLSubObjectPropertyOfAxiom axiom : superObjectPropertyAxioms.toArray(OWLSubObjectPropertyOfAxiom[]::new)) {
+                    //if(axiom.getSuperProperty().getClassExpressionType().getName().equals("Class")) {
+                        Stream<OWLObjectProperty> superObjectProperties = axiom.getSuperProperty().objectPropertiesInSignature();
+                        for (OWLObjectProperty superobjprop : superObjectProperties.toArray(OWLObjectProperty[]::new)) {
+                            OwlObjectPropertyNode superobjpropnode = new OwlObjectPropertyNode();
+                            superobjpropnode.create(superobjprop, this.owlontology);
+                            if (!superobjpropnode.name.equals("topObjectProperty")) {
+                                istoplevel.set(false);
+                                break;
+                            }
+                        };
+                    //}
+                };
+                if (istoplevel.get()) {
+                    if (objpropnode != null) {
+                        this.children.add(objpropnode);
+                    }
+                }
+            }
+        }
+        for (OwlObjectPropertyNode toplevelnode : this.children) {
+            toplevelnode.addSubObjectProperties();
+        }
+    }
+}
