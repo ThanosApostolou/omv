7c89d18156da239f1db158f9a4894ae51c7dbbdd
thanosapostolou <thanosapostolou@outlook.com>
2020-10-10 14:50:11 +0300

    add reference model metrics

--- omv-client/src/gui/views/Visualizer/VisualizationStatisticsComp.vue
+++ omv-client/src/gui/views/Visualizer/VisualizationStatisticsComp.vue
@@ -14,12 +14,71 @@
                     <p>There are {{ statistics.numberofPropRules }} ({{ statistics.percentPropRules }}%) Properties Rules which connect classes, object properties and data properties with each ontology.</p>
                 </v-card-text>
             </v-card>
-            <v-card v-if="isReference" outlined>
+            <v-card outlined>
                 <v-card-title>
                     Reference Model
                 </v-card-title>
                 <v-card-text>
-                    <p>asdfasd</p>
+                    <p>
+                        Metrics about top level classes of right Ontology, which is usually used as a reference model:
+                    </p>
+                    <div v-for="(child1, index1) in owl2.owlclasses.children" :key="index1">
+                        <v-row v-if="child1.totalclassrules > 0 || child1.totalproprules > 0">
+                            <v-col cols="3" md="2">
+                                <b>"{{ child1.label }}"</b> has:
+                            </v-col>
+                            <v-col cols="3" md="2">
+                                <v-row>
+                                    {{ child1.totalclassrules }} Class Rules
+                                </v-row>
+                                <v-row>
+                                    {{ child1.totalproprules }} Properties Rules
+                                </v-row>
+                                <v-row v-if="child1.children.length > 0">
+                                    of which:
+                                </v-row>
+                            </v-col>
+                            <v-col cols="3" md="2" />
+                            <v-col cols="3" md="2" />
+                        </v-row>
+                        <div v-for="(child2, index2) in child1.children" :key="index2">
+                            <v-row v-if="child2.totalclassrules > 0 || child2.totalproprules > 0">
+                                <v-col cols="3" md="2" />
+                                <v-col cols="3" md="2">
+                                    <b>"{{ child2.label }}"</b> has:
+                                </v-col>
+                                <v-col cols="3" md="2">
+                                    <v-row>
+                                        {{ child2.totalclassrules }} Class Rules
+                                    </v-row>
+                                    <v-row>
+                                        {{ child2.totalproprules }} Properties Rules
+                                    </v-row>
+                                    <v-row v-if="child2.children.length > 0">
+                                        of which:
+                                    </v-row>
+                                </v-col>
+                                <v-col cols="3" md="2" />
+                            </v-row>
+                            <div v-for="(child3, index3) in child2.children" :key="index3">
+                                <v-row v-if="child3.totalclassrules > 0 || child3.totalproprules > 0">
+                                    <v-col cols="3" md="2" />
+                                    <v-col cols="3" md="2" />
+                                    <v-col cols="3" md="2">
+                                        <b>"{{ child3.label }}"</b> has:
+                                    </v-col>
+                                    <v-col cols="3" md="2">
+                                        <v-row>
+                                            {{ child3.totalclassrules }} Class Rules
+                                        </v-row>
+                                        <v-row>
+                                            {{ child3.totalproprules }} Properties Rules
+                                        </v-row>
+                                    </v-col>
+                                </v-row>
+                            </div>
+                        </div>
+                    </div>
                 </v-card-text>
             </v-card>
             <v-card outlined>
@@ -70,11 +129,6 @@ export default {
             owl1: this.statistics.visualization.owl1,
             owl2: this.statistics.visualization.owl2
         };
-    },
-    computed: {
-        isReference() {
-            return this.owl2.iri == "http://www.semanticweb.org/ntua/iccs/harmonicss/terminology";
-        }
     }
 
 };
